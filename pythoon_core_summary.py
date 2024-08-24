18b0f25b9bbb12cba6aeb840b70320508c856446
sharppink <kim0004102@gmail.com>
2024-08-24 18:16:01 +0900

    Create pythoon_core_summary.py

---
+++ pythoon_core_summary.py
@@ -0,0 +1,57 @@
+#파이썬 기본
+#출력
+
+# print('hello python')
+
+#숫자계산
+
+# print(3+7) #+
+# print(7-3) #-
+# print(3*7) #*
+# print(3/7) #/
+# print(7/3) #나눗셈
+# print(7//3) #몫
+# print(7%3) #나머지
+# print(7**3) #거듭제곱
+
+#변수
+
+# print('hello python')
+# str_data = "hello"
+# int_data = 1
+# float_data = 1.1
+# bool_data = True
+# print(str_data, int_data, float_data, bool_data)
+
+#변수의 활용
+
+# num1 = 14
+# num2 = 5
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+
+#small project2
+
+# r = 10
+# pi = 3.1415
+# print(2*r*pi) #둘레
+# print(pi*r**2) #넓이
+
+#small project3
+
+# age = int(input("나이는?"))
+# if age >= 19:
+#     print('성인 입니다.')
+# else:
+#     print('미성년자')
+
+#set 계산
+
+# a = {1,2,3}
+# b = {2,3,4}
+# print(a&b) #교집합
+# print(a-b) #a-b
+# print(a^b) #a합집합b - a교집합b
+# print(a|b) #합집합
