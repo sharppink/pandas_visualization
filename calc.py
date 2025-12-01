14cf2162fd7eca4ab7bcccf971b8e1d762141955
sharppink <kim0004102@gmail.com>
2025-12-01 23:24:42 +0900

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,10 +1,26 @@
+#!pip install datetime
+#!pip install pykrx
+#!pip install tensorflow
+#!pip install sklearn
+#!pip install matplotlib
 import sys
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlob.dates as mdates
+from sklearn.keras.models import Model
+from tensorflow.keras.layers import(
+    Input, Dense, Dropout, LayerNormalization, MultiHeadAttention, Add, GlobalAveragePooling1D
+)
+from tensorflow.keras.optimizers import Adam
+from datetime import datetime
 from pykrx import stock
 
-
-
+#005930
 value = sys.argv[1]  
 
-df = stock.get_market_ohlcv_by_date(fromdate="20200101", todate="20241231", ticker="005930")
-print(df)
+result = stock.get_market_ohlcv_by_date(fromdate="20200101", todate="20241231", ticker=value)
 
+print(result.to_csv(index=True))
+
+def build_transformer
